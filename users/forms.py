9a849496cd6ea16454ef576c83a00517101171d4
Hitoshi-Hub <urupinwalter@gmail.com>
2026-05-17 10:22:01 +0300

    Second fix registration form

--- users/forms.py
+++ users/forms.py
@@ -31,10 +31,30 @@ class StudentRegistrationForm(UserCreationForm):
         try:
             validate_password(password1, self.instance)
         except ValidationError as exc:
-            raise ValidationError([
-                f'Пароль не подходит: {message}' for message in exc.messages
-            ])
+            translated_messages = []
+            for error in exc.error_list:
+                if error.code == 'password_too_short':
+                    translated_messages.append('Пароль слишком короткий. Минимум 8 символов.')
+                elif error.code == 'password_too_common':
+                    translated_messages.append('Пароль слишком простой. Выберите более сложный вариант.')
+                elif error.code == 'password_entirely_numeric':
+                    translated_messages.append('Пароль не может состоять только из цифр.')
+                elif error.code == 'password_too_similar':
+                    translated_messages.append('Пароль слишком похож на ваши личные данные.')
+                else:
+                    translated_messages.append(str(error))
+            raise ValidationError([f'Пароль не подходит: {message}' for message in translated_messages])
         return password1
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password2_errors = self.errors.as_data().get('password2', [])
+        mismatch_errors = [error for error in password2_errors if error.code == 'password_mismatch']
+        if mismatch_errors:
+            self._errors['password2'] = self.error_class(['Пароли не совпадают.'])
+        elif 'password2' in self._errors:
+            self._errors.pop('password2', None)
+        return cleaned_data
 
     def save(self, commit=True):
         user = super().save(commit=False)
